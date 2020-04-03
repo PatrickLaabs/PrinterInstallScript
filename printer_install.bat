0966291ac97cc1c01e7293967ec51beae48b00f2
PatrickLaabs <patrick.laabs@me.com>
2020-04-03 10:59:43 +0200

    Changes to the Windows Script; variables weren't set right; Inserted a resolution for relative paths on windows

--- printer_install.bat
+++ printer_install.bat
@@ -8,7 +8,7 @@ SET varDriver="TOSHIBA Generic Printer XL"
 
 :: Path to the .inf file - in this case, the driver is located inside the same folder as this script
 :: in this case, you don't need to use the full path - just starting from the root
-SET varDriverPath="Toshiba Generic Driver 2.13.1.0 XL\x86\Drivers\Print\GDI\TSUD1p40.inf"
+SET varDriverPath="%~dp0\Toshiba Generic Driver 2.13.1.0 XL\x86\Drivers\Print\GDI\TSUD1p40.inf"
 
 :: Set your printer name that will be displayed to the user
 SET varDisplayName="Printer Display Name"
@@ -17,4 +17,4 @@ SET varDisplayName="Printer Display Name"
 CSCRIPT /nologo %windir%\System32\Printing_Admin_Scripts\de-DE\prnport.vbs -a -r %varIp% -o raw -n 9100 -h %varIp%
 
 :: Install printer
-rundll32 printui.dll,PrintUIEntry /if /n %varDisplayName% /b %varDisplayName% /f %varDriverPath% /r %varIp% /m %varDriverPath%
+rundll32 printui.dll,PrintUIEntry /if /n %varDisplayName% /b %varDisplayName% /f %varDriverPath% /r %varIp% /m %varDriver%
